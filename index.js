eede0636d81a4e14c7d26f220ef489c77597d2af
Ally Netzel <netzelally@gmail.com>
2018-11-30 01:38:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+const logDriverNames = function (drivers) {
+
+  drivers.forEach(function (driver) {
+  console.log(driver.name);
+  });
+};
+
+const logDriversByHometown = function(drivers, location){
+    drivers.forEach(function (driver) {
+      if (location === driver.hometown){
+        console.log(driver.name);
+      }
+    });
+};
+
+function driversByRevenue(drivers) {
+  return drivers.slice().sort(function (a, b) {
+    return a.revenue - b.revenue
+  })
+}
+
+const driversByName = function(drivers) {
+  return drivers.slice().sort(function(driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
+  });
+};
+
+const totalRevenue = function(drivers){
+  let total = 0;
+  drivers.forEach(function(driver){
+    total += driver.revenue;
+  });
+  return total;
+}
+
+
+const averageRevenue = function(drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
