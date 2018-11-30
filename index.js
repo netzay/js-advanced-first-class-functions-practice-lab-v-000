e5ead853b8e3830f9606ce4c2b7b5544365ca1e1
Ally Netzel <netzelally@gmail.com>
2018-11-30 00:55:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const logDriverNames = function (drivers) {
+
+  drivers.forEach(function (driver) {
+  console.log(driver.name);
+  });
+};
+
+const logDriversByHometown = function(drivers, location){
+    drivers.forEach(function (driver) {
+      if (location === driver.hometown){
+        console.log(driver.name);
+      }
+    });
+};
+
+const driversByRevenue = function(drivers){
+    const newDrivers = []  
+    drivers.revenue.sort(function (a, b) {
+  return a.localeCompare(b);
+});
+   
+  drivers.forEach(function (driver) {
+    newDrivers.push(driver.revenue.sort);
+  });
+    return newDrivers;
+}
